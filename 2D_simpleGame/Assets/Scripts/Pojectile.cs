aac72eb7158867737897d372411dddbef00e6eb5
anthony bardsley <anthony.d.bardsley@gmail.com>
2018-10-11 10:30:54 -0600

    ranged weapon

---
+++ 2D_simpleGame/Assets/Scripts/Pojectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pojectile : MonoBehaviour {
+
+	public float speed;
+
+	public Rigidbody2D PC;
+
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectileParticle;
+
+	public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+
+		if (PC.transform.localScale.x < 0)
+		{
+			speed = -speed;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+		
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Enemy"){
+			Instantiate(EnemyDeath,other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreManager.Addpoints(PointsForKill);
+		}
+		
+		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+
+}
